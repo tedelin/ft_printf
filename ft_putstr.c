63284f6ee16714bb17adb0031803d3803f4d08d6
Tanguy EDELIN <tedelin@e3r13p5.clusters.42paris.fr>
2022-12-21 15:09:08 +0100

    optimization of ft_putstr

--- ft_putstr.c
+++ ft_putstr.c
@@ -6,21 +6,26 @@
 /*   By: tedelin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 03:11:44 by tedelin           #+#    #+#             */
-/*   Updated: 2022/11/30 03:11:45 by tedelin          ###   ########.fr       */
+/*   Updated: 2022/12/21 15:05:55 by tedelin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <unistd.h>
 
-int	ft_putstr(char *str)
+int	ft_strlen(char *str)
 {
 	int	i;
 
-	i = 0;
 	if (!str)
-		return (write(1, "(null)", 6));
-	while (str && str[i])
-		ft_putchar(str[i++]);
+		return (0);
+	i = 0;
+	while (str[i])
+		i++;
 	return (i);
+}
+
+int	ft_putstr(char *str)
+{
+	return (write(1, str, ft_strlen(str)));
 }
